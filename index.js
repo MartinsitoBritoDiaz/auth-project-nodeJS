2c63a80e22605b7b549c1a601d7996ece7e02d9c
Martinsito <martinsitobd@gmail.com>
2022-08-04 21:26:06 -0400

    Method post was created (API REST)

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const express = requier('express');
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
+const User = require('./user');
+
+mongoose.connect('mongodb+srv://martinsitobd:RwbBI8ZIdZFB12jy@cluster0.4qe63.mongodb.net/auth?retryWrites=true&w=majority');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+    const { body } = req;
+    console.log({ body });
+
+    try{
+        const isUser = await User.findOne({ email: body.email });
+        
+        if(isUser){
+            return res.status(403).send('User is already created');
+        }
+
+        const salt = await bcrypt.genSalt();
+        const hashed = await bcrypt.hash(body.password, salt);
+        const user = await User.create( { email:body.email, password: hashed, salt });
+
+        res.send( { _id: user._id } );
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+});
+
+
+app.listen(3000, () =>{
+    console.log('Listening in port 3000');
+})
