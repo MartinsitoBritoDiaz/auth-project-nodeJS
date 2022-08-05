77e8fc550d72e0de78d1c5d0b4eba72ffbd07606
Martinsito <martinsitobd@gmail.com>
2022-08-04 22:54:31 -0400

    End point get was created sucessfuly

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
+const jsonWebToken = require('jsonwebtoken');
+const { expressjwt: jwt} = require('express-jwt');
 const User = require('./user');
 
 mongoose.connect('mongodb+srv://martinsitobd:RwbBI8ZIdZFB12jy@cluster0.4qe63.mongodb.net/auth?retryWrites=true&w=majority');
@@ -10,7 +10,12 @@ mongoose.connect('mongodb+srv://martinsitobd:RwbBI8ZIdZFB12jy@cluster0.4qe63.mon
 const app = express();
 
 app.use(express.json());
+const signToken = _id => jsonWebToken.sign({ _id }, process.env.SECRET);
 
+//Middleware for validating a jwt
+const validatedJwt = jwt( {secret: process.env.SECRET, algorithms: ['HS256'] });
+
+//End Point to register
 app.post('/register', async (req, res) => {
     const { body } = req;
     console.log({ body });
@@ -25,13 +30,62 @@ app.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt();
         const hashed = await bcrypt.hash(body.password, salt);
         const user = await User.create( { email:body.email, password: hashed, salt });
-
-        res.send( { _id: user._id } );
+        
+        const signed = signToken(user._id);
+        
+        res.status(201).send(signed);
 
     }catch(err){
         console.log(err);
         res.status(500).send(err.message);
     }
+});
+
+//End Point to login
+app.post('/login', async (req,res) => {
+    const { body } = req;
+    const user = await User.findOne( {email: body.email});
+
+    if(!user){
+        res.status(403).send('User and password not found');
+    }else {
+        const isMatch = await bcrypt.compare(body.password, user.password);
+
+        if(isMatch){
+            const signed = signToken(user._id);
+            res.status(200).send(signed);
+        }else{
+            res.status(403).send('User and password not found');
+        }
+    }
+
+    try{
+
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+});
+
+const findAndAssignUser =  async (req, res, next) => {
+    try{
+        const user = await User.findById(req.auth);
+
+        if(!user){
+            return res.status(401).end();
+        }
+
+        req.user = user;
+        next();
+    }catch(e){
+        next(e);
+    }
+}
+
+const isAuthenticated = express.Router().use(validatedJwt, findAndAssignUser);
+
+//End point 
+app.get('/main', isAuthenticated, (req, res, next) => {
+    res.send(req.user);
 });
 
 
